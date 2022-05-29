e52d8804898c39bfefdc6ce5c857bcf1a0be2774
Eddie Lee <eddielee@EddiedeMacBook-Pro.local>
2022-05-29 15:22:10 +0800

    init

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/line/line-bot-sdk-go/linebot"
+)
+
+var (
+	client *linebot.Client
+	err    error
+)
+
+func main() {
+	// 建立客戶端
+	client, err = linebot.New(os.Getenv("CHANNEL_SECRET"), os.Getenv("CHANNEL_ACCESS_TOKEN"))
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	http.HandleFunc("/callback", callbackHandler)
+
+	log.Fatal(http.ListenAndServe(":84", nil))
+}
+
+func callbackHandler(w http.ResponseWriter, r *http.Request) {
+	// 接收請求
+	events, err := client.ParseRequest(r)
+
+	if err != nil {
+		if err == linebot.ErrInvalidSignature {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(500)
+		}
+
+		return
+	}
+
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.TextMessage:
+				// 回覆訊息
+				if _, err = client.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+					log.Println(err.Error())
+				}
+			}
+		}
+	}
+}
